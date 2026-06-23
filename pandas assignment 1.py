1113ca484b3508f6bee1abcb2bc8dc38195e2f81
vaishnavikhalbate-Prabha <vaishnavikhalbateprabha@gmail.com>
2026-06-23 23:21:16 +0530

    pandas assignment 1.py

--- pandas assignment 1.py
+++ pandas assignment 1.py
@@ -1,11 +1,31 @@
 import pandas as pd
+
+#Create Dataframe
+
 Data = {
     "Name":[ "vaishnavi", "saii", "shravani","shreya"],
 
     "Age": [21 , 20 , 16 , 19],
 
-    "City": ["Pune" ,"Mumbaii" ,"Nashik" , "Banglore"]  
+    "City": ["Pune" ,"Mumbaii" ,"Nashik" , "Banglore"],  
+
+    "marks": [87 , 79 ,95, 65]
+
         }
+
+#Display Dtaframe
+
 df = pd.DataFrame(Data)
+print(df)
+
+#Save Datframe to csv
+
+df.to_csv("student.csv",index=False)
+print("\n Data saved to student.csv")
+print()
+
+
+#Read csv file
+df = pd.read_csv("student.csv")
 
 print(df)
